e95b33603c8ebca82f95d7d0e6bdc7e0f05b1c26
Will Fong <willfong@hey.com>
2023-08-08 22:32:21 +0700

    Init reporting dates

--- src/components/ReportDetails.js
+++ src/components/ReportDetails.js
@@ -14,37 +14,29 @@ export default function SpendDetailPage({ purchases, categories }) {
 	}, {});
 
 	return (
-		<main className="px-3 py-2 lg:flex-auto lg:px-0 lg:py-20">
-			<div className="mx-auto max-w-2xl space-y-16 sm:space-y-20 lg:mx-0 lg:max-w-none ">
-				<div>
-					<nav className="h-full overflow-y-auto mt-4">
-						{Object.keys(purchases).map((day) => (
-							<div key={day} className="relative">
-								<div className="sticky top-0 z-10 bg-zinc-200 px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 flex justify-between">
-									<h3>{format(new Date(day), "EEE, MMM dd yyyy")}</h3>
-									<div className="text-gray-500">Total: {dayTotals[day].toLocaleString()}</div>
+		<nav className="h-full overflow-y-auto mt-4">
+			{Object.keys(purchases).map((day) => (
+				<div key={day} className="relative">
+					<div className="sticky top-0 z-10 bg-zinc-200 px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 flex justify-between">
+						<h3>{format(new Date(day), "EEE, MMM dd yyyy")}</h3>
+						<div className="text-gray-500">Total: {dayTotals[day].toLocaleString()}</div>
+					</div>
+					<ul className="divide-y divide-gray-100">
+						{purchases[day].map((item) => (
+							<li key={item.id} className="flex gap-x-4 px-3 py-5 bg-white">
+								<div className="flex-none flex gap-x-2 items-center">
+									<p className="text-gray-500">{format(item.date, "HH:MM")}</p>
+									<p className="text-gray-500">{categoriesLookup[item.categoryId]["name"]}</p>
 								</div>
-								<ul className="divide-y divide-gray-100">
-									{purchases[day].map((item) => (
-										<li key={item.id} className="flex gap-x-4 px-3 py-5 bg-white">
-											<div className="flex-none flex gap-x-2 items-center">
-												<p className="text-gray-500">{format(item.date, "HH:MM")}</p>
-												<p className="text-gray-500">{categoriesLookup[item.categoryId]["name"]}</p>
-											</div>
-											<div className="flex-1 text-gray-500 overflow-hidden whitespace-nowrap">
-												<p className="truncate">{item.message}</p>
-											</div>
-											<div className="flex-none flex-shrink-0 text-gray-900 text-right">
-												{item.amount.toLocaleString()}
-											</div>
-										</li>
-									))}
-								</ul>
-							</div>
+								<div className="flex-1 text-gray-500 overflow-hidden whitespace-nowrap">
+									<p className="truncate">{item.message}</p>
+								</div>
+								<div className="flex-none flex-shrink-0 text-gray-900 text-right">{item.amount.toLocaleString()}</div>
+							</li>
 						))}
-					</nav>
+					</ul>
 				</div>
-			</div>
-		</main>
+			))}
+		</nav>
 	);
 }
--- src/pages/Report.js
+++ src/pages/Report.js
@@ -1,19 +1,35 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { format } from "date-fns";
+import { format, parseISO, startOfMonth, endOfMonth } from "date-fns";
+import { Switch } from "@headlessui/react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 import { purchasesGetByMonth, categoriesGetAll } from "../idb";
 import ReportSummary from "../components/ReportSummary";
 import ReportDetails from "../components/ReportDetails";
 
+function classNames(...classes) {
+	return classes.filter(Boolean).join(" ");
+}
+
 export default function ReportPage() {
 	const navigate = useNavigate();
+	const [showMonth, setShowMonth] = useState(0);
+	const [customStart, setCustomStart] = useState(new Date());
+	const [customEnd, setCustomEnd] = useState(new Date());
+	const [enabled, setEnabled] = useState(false);
 	const [purchases, setPurchases] = useState([]);
 	const [categories, setCategories] = useState([]);
 	const [summary, setSummary] = useState({});
 	const [dataLoaded, setDataLoaded] = useState(false);
 
 	useEffect(() => {
+		const currentDate = new Date();
+		setShowMonth(currentDate.getMonth());
+		setCustomStart(startOfMonth(currentDate));
+		setCustomEnd(endOfMonth(currentDate));
+
 		const fetchCategories = async () => {
 			const data = await categoriesGetAll();
 			if (data.length === 0) {
@@ -25,6 +41,31 @@ export default function ReportPage() {
 			setDataLoaded(true);
 		});
 	}, [navigate]);
+
+	const getCurrentMonthly = () => {
+		//TODO: Change year as well
+		const currentDate = new Date();
+		currentDate.setMonth(showMonth);
+		const dateString = format(currentDate, "yyyy-MM-dd");
+		const parsedDate = parseISO(dateString);
+		const monthName = format(parsedDate, "MMMM yyyy");
+		return monthName;
+	};
+
+	const setPreviousMonth = () => {
+		if (showMonth === 0) {
+			setShowMonth(11);
+		} else {
+			setShowMonth(showMonth - 1);
+		}
+	};
+	const setNextMonth = () => {
+		if (showMonth === 11) {
+			setShowMonth(0);
+		} else {
+			setShowMonth(showMonth + 1);
+		}
+	};
 
 	const fetchPurchases = async () => {
 		const currentDate = new Date();
@@ -51,13 +92,56 @@ export default function ReportPage() {
 	if (!dataLoaded) return null;
 
 	return (
-		<div className="py-10">
-			<header>
-				<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-					<h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">Report</h1>
+		<div className="py-4 px-4 inline-block min-w-full align-middle">
+			<header className="flex justify-between">
+				<div className="">
+					{!enabled && (
+						<div className="text-2xl">
+							{getCurrentMonthly()} <span onClick={setPreviousMonth}>Prev</span> |{" "}
+							<span onClick={setNextMonth}>Next</span>
+						</div>
+					)}
+					{enabled && (
+						<>
+							Start: <DatePicker selected={customStart} onChange={(date) => setCustomStart(date)} />
+							End: <DatePicker selected={customEnd} onChange={(date) => setCustomEnd(date)} />
+						</>
+					)}
+				</div>
+				<div className="items-center">
+					<Switch.Group as="div" className={"flex items-center flex-col sm:flex-row"}>
+						<Switch.Label
+							as="span"
+							className={classNames(!enabled ? "font-semibold text-gray-500" : "text-gray-400", "md:mr-3 text-sm")}
+						>
+							<span>Monthly</span>
+						</Switch.Label>
+						<Switch
+							checked={enabled}
+							onChange={setEnabled}
+							className={classNames(
+								enabled ? "bg-zinc-400" : "bg-zinc-400",
+								"relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out"
+							)}
+						>
+							<span
+								aria-hidden="true"
+								className={classNames(
+									enabled ? "translate-x-5" : "translate-x-0",
+									"pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out"
+								)}
+							/>
+						</Switch>
+						<Switch.Label
+							as="span"
+							className={classNames(enabled ? "font-semibold text-gray-500" : "text-gray-400", "md:ml-3 text-sm ")}
+						>
+							<span>Custom</span>
+						</Switch.Label>
+					</Switch.Group>
 				</div>
 			</header>
-			<main>
+			<main className="mt-8">
 				<ReportSummary categories={categories} summary={summary} />
 				<ReportDetails categories={categories} purchases={purchases} />
 			</main>
