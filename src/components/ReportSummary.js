21b7ef4cc3008f74422fff94d86653d7a0667f76
Will Fong <willfong@hey.com>
2023-08-09 12:07:56 +0700

    Add monthly total

--- src/components/ReportSummary.js
+++ src/components/ReportSummary.js
@@ -1,16 +1,24 @@
 export default function ReportSummary({ categories, summary }) {
+	const grandTotal = Object.values(summary).reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
 	return (
 		<div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
 			<table className="min-w-full divide-y divide-gray-300">
 				<tbody className="divide-y divide-gray-200 bg-white">
 					{categories.map((c) => (
 						<tr key={c.id}>
-							<td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">{c.name}</td>
-							<td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+							<td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm  text-gray-700 sm:pl-6">{c.name}</td>
+							<td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-gray-700 text-sm sm:pr-6">
 								{summary[c.id]?.toLocaleString() ?? 0}
 							</td>
 						</tr>
 					))}
+					<tr key="grandTotal">
+						<td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-semibold text-gray-900 sm:pl-6">Total</td>
+						<td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm text-gray-900 sm:pr-6">
+							{grandTotal.toLocaleString()}
+						</td>
+					</tr>
 				</tbody>
 			</table>
 		</div>
